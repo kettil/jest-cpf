63c5637863ed6187bbf38d9ea3dc1826fddf80dd
Kettil <development@kettil.de>
2020-02-19 02:36:34 +0100

    Add a eslint comment line

--- src/bin/jest-cpf.ts
+++ src/bin/jest-cpf.ts
@@ -3,7 +3,7 @@
 /* eslint-disable import/no-unused-modules, unicorn/filename-case */
 
 import { argv } from 'yargs';
-import { outputError } from '../lib/outputs';
 import main from '../lib/app';
+import { outputError } from '../lib/outputs';
 
 main(process.argv.splice(2), argv).catch(outputError);
